d0f44c7414862e000320a86b3dc52a305375cf35
Jademir de Moura <jademirf@gmail.com>
2021-05-13 20:19:56 -0300

    aula de localStorage

--- js/global.js
+++ js/global.js
@@ -1,1 +1,14 @@
-document.write('<h1>Título da página!</h1>')
+let usuario = null
+
+
+//setando um item com chave "username" e valor "Desmennyellysson"
+// window.localStorage.setItem('username', 'Desmennyellysson') // window.localStorage.username = 'Desmennyellysson'
+
+if (!window.localStorage.username) {
+  usuario = window.prompt('Por favor informe seu nome:')
+  window.localStorage.setItem('username', usuario)
+} else {
+  usuario = window.localStorage.username // window.localStorage.getItem('username')
+}
+
+document.write(`<h1>Olá ${usuario}</h1>`)
